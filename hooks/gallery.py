cbfd66c0da72007e065c8c5d86dd1ec0e500f973
Jonathan Goodson <jgoodson@umd.edu>
2015-02-18 17:33:11 -0500

    changing old lab member page to use individual pages, adding cookie competition images

--- hooks/gallery.py
+++ hooks/gallery.py
@@ -18,7 +18,7 @@ class Album(object):
     def __init__(self, folder):
         self.folder = folder
         self.images = [Image(image,folder) for image in os.listdir(folder) if any(image.endswith(ftype) for ftype in FILE_TYPES) and not image.startswith(THUMB_PREFIX)]
-        self.name = os.path.basename(folder)
+        self.name = os.path.basename(folder).lstrip('01234567890')
 
 
 
@@ -29,11 +29,13 @@ def get_albums(config, page, templ_vars):
     """
     if 'type' in page.meta and page.meta['type'] == 'gallery':
 
-        # for each album
         albums = []
         gallery_folder = os.path.join(page.options['output_dir'],'img','gallery')
-        for folder in os.listdir(gallery_folder):
-            albums.append(Album(os.path.join(gallery_folder,folder)))
+        existing = set()
+        with open(gallery_folder+'/order.txt') as arrangement:
+            for line in arrangement:
+                albums.append(Album(os.path.join(gallery_folder,line.strip())))
+                existing.add(line.strip())
 
         templ_vars['site']['albums'] = albums
 
